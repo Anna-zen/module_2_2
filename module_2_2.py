fe7f18c3dc78f6d5612748bc1c6ed804869bd571
Anna Zen <annadavidson7777@gmail.com>
2024-09-23 00:44:45 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = input('Введите первое число: ')
+second = input('Введите второе число: ')
+third = input('Введите третье число: ')
+if first == second and first == third:
+    print ('Одинаковых чисел: 3')
+elif first == second or first == third or second == third:
+    print('Одинаковых чисел: 2')
+else :
+    print('Одинаковых чисел: 0')
